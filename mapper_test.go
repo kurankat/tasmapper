6b73c105114cc0d7aca0ebad944e018c2d7cde2f
kurankat <dr.microbe@gmail.com>
2021-11-08 12:36:39 +1100

    Comment out unused test functions

--- mapper_test.go
+++ mapper_test.go
@@ -37,23 +37,23 @@ var mixedVouchered string = `-42.63341,147.75224,v
 -41.34221,145.43442,a
 41,24,,145,24,,v`
 
-// assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
-	if !condition {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
-	}
-}
+// // assert fails the test if the condition is false.
+// func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+// 	if !condition {
+// 		_, file, line, _ := runtime.Caller(1)
+// 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+// 		tb.FailNow()
+// 	}
+// }
 
-// ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
-	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
-	}
-}
+// // ok fails the test if an err is not nil.
+// func ok(tb testing.TB, err error) {
+// 	if err != nil {
+// 		_, file, line, _ := runtime.Caller(1)
+// 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+// 		tb.FailNow()
+// 	}
+// }
 
 // equals fails the test if exp is not equal to act.
 func equals(tb testing.TB, exp, act interface{}) {
